8b5c69d351685ff92832910ff82718e826989bc1
Shamadruu <jacksonayres11@gmail.com>
2016-05-24 14:38:07 -0600

    Update mapper.js
    Added filtering by resource tiers.

--- mapper.js
+++ mapper.js
@@ -27,11 +27,6 @@
 	}
 	if(localStorage.getItem("data")){
 		map = load();
-		for(var t in map){ 
-			var tile = map[t]; 
-			tile.centerX = tile.x+tile.y/2;
-			tile.centerY = tile.y;
-		}
 	}
 	if(document.getElementById("mapper")){
 		document.getElementById("mapper").remove();
@@ -72,14 +67,14 @@
 	function log() {
 		$("#map > g").find("g").each(function(){ 
 			var tile = {};
-			var id = $(this).attr("id");
+			id = $(this).attr("id");
 			if(!map[id]){
 				map[id] = tile;
 			}
 			
 			var coords = $(this).attr("id").replace(/x(?=\d+)|y(?=\d+)/g, " ").trim().split(" ");
-			tile.x = Number(coords[0]);
-			tile.y = Number(coords[1]);
+			tile.x = ~~coords[0];
+			tile.y = ~~coords[1];
 			tile.centerX = tile.x+tile.y/2;
 			tile.centerY = tile.y;
 			
@@ -94,19 +89,19 @@
 			tile.monsters = map[id].monsters;
 			
 			if(text.indexOf("Tier 1") !== -1){
-				tile.t1 = text.replace(/Tier [12345]|%/g, "");
+				tile.t1 = ~~text.replace(/Tier [12345]|%/g, "");
 			}
 			else if(text.indexOf("Tier 2") !== -1){
-				tile.t2 = text.replace(/Tier [12345]|%/g, "");
+				tile.t2 = ~~text.replace(/Tier [12345]|%/g, "");
 			}
 			else if(text.indexOf("Tier 3") !== -1){
-				tile.t3 = text.replace(/Tier [12345]|%/g, "");
+				tile.t3 = ~~text.replace(/Tier [12345]|%/g, "");
 			}
 			else if(text.indexOf("Tier 4") !== -1){
-				tile.t4 = text.replace(/Tier [12345]|%/g, "");
+				tile.t4 = ~~text.replace(/Tier [12345]|%/g, "");
 			}
 			else if(text.indexOf("Tier 5") !== -1){
-				tile.t5 = text.replace(/Tier [12345]|%/g, "");
+				tile.t5 = ~~text.replace(/Tier [12345]|%/g, "");
 			}
 			else if(text.search(/\d+,\d+/) === -1 && text !== ""){
 				tile.monsters = uconcat(tile.monsters.concat(text.replace(/ +/g, "").split(",")));
@@ -129,7 +124,7 @@
 			tile.points += ' ' + [(((tile.centerX + 1)*s*2)-s), (((tile.centerY+1)*3*s/2)+s/2)].join(",");
 			tile.points += ' ' + [(((tile.centerX + 1)*s*2)-s), (((tile.centerY+1)*3*s/2)-s/2)].join(",");
 		
-			g +='<g id="' + t + '" name="' + t + '" class="' + tile.type + ' ' + tile.kingdom + '"><polygon name="' + t + '" points="' + tile.points +'"></polygon><text><tspan name="' + t + '" x="' + ((tile.centerX+1)*s*2) + '" y="' + ((tile.centerY+1)*3*s/2) + '">'  + tile.x + ',' + tile.y +  '</tspan></text></g>';
+			g +='<g id="' + t + '" name="' + t + '" class="' + tile.type + ' ' + tile.kingdom + '"><polygon name="' + t + '" points="' + tile.points +'"></polygon><text><tspan name="' + t + '" x="' + ((tile.centerX+1)*s*2) + '" y="' + ((tile.centerY+1)*3*s/2) + '">'  + tile.x + ',' + tile.y +  '</tspan><tspan name="' + t + '" x="' + ((tile.centerX+1)*s*2) + '" y="' + (s/3+(tile.centerY+1)*3*s/2) + '">' + (tile.t1||"??") + '|' + (tile.t2||"??") +'|' +(tile.t3||"??") +'|' + (tile.t4||"??") +'|' + (tile.t5||"?") + '</tspan></g>';
 		}
 		group.innerHTML = g;
 	}
@@ -137,12 +132,12 @@
 		var size = 10;
 		var scale = 1;
 		var head = newWindow.document.getElementsByTagName("head")[0];
-			head.innerHTML = "<style>body { background: #666666; margin:0;} #newMap { width: 100%; height: 90%; vertical-align:bottom; background-color: #666666;} #navbar { width: 100%; height: 10%; background-color: white;vertical-align: top; position:relative;} polygon {stroke: hsl(0, 0%, 70%); stroke-width: 0.01%;} g > text {text-anchor:middle;font-size: 5.5px; font-stretch: extra-condensed;} .plain polygon{fill: hsl(72, 87%, 71%);} .city polygon{fill: hsl(43, 26%, 46%);} .lake polygon{fill:hsl(231, 91%, 62%);} .forest polygon{fill:hsl(108, 26%, 32%);} .swamp polygon{fill:hsl(72, 8%, 31%);} .rock polygon{fill: hsl(172, 4%, 46%);} .Kg1 polygon, .Kg2 polygon {stroke: hsl(240, 0%, 100%) !important; stroke-width: 0.05% !important;}}";
+			head.innerHTML = "<style>body { background: #666666; margin:0;} #newMap { width: 100%; height: 90%; vertical-align:bottom; background-color: #666666;} #navbar { width: 100%; height: 10%; background-color: white;vertical-align: top; position:relative;} polygon {stroke: hsl(0, 0%, 70%); stroke-width: 0.01%;} g > text {text-anchor:middle;font-size: 5.5px; font-stretch: extra-condensed;} g > text > tspan:nth-of-type(2){ font-size: 0.65em;} .plain polygon{fill: hsl(72, 87%, 71%);} .city polygon{fill: hsl(43, 26%, 46%);} .lake polygon{fill:hsl(231, 91%, 62%);} .forest polygon{fill:hsl(108, 26%, 32%);} .swamp polygon{fill:hsl(72, 8%, 31%);} .rock polygon{fill: hsl(172, 4%, 46%);} .Kg1 polygon, .Kg2 polygon {stroke: hsl(348, 83%, 47%) !important; stroke-width: 0.05% !important;}}";
 			
 			//If the navbar does not exist, assume that this is an entirely new window.
 			if(!newWindow.document.getElementById("navbar")){
 				newWindow.document.getElementsByTagName("body")[0].innerHTML = '<div id="navbar"></div><div><svg id="newMap"><g></g></svg></div>';
-				newWindow.document.getElementById("navbar").innerHTML = '<table style="height: 100%;display: inline-block;position: absolute;left: 5%;"> <thead> <tr> <th colspan="2">Target Tile</th> <th colspan="2">Scale</th> </tr> </thead> <tbody> <tr> <th>X-Coord: </th> <td><input id="tileX" type="number" value="0"></td> <th>Scale: </th> <td><input id="scale" type="number" value="1"></td> </tr> <tr> <th>Y-Coord: </th> <td><input id="tileY" type="number" value="0"></td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> </tbody> </table> <table id="data" style="height: 100%;top: 0px;display: inline-block;position: absolute;right: 5%;"> <thead> <tr> <th colspan="6">Tile Data</th> </tr> </thead> <tbody> <tr> <th>Coords: </th> <td>(XXX, YYY)</td> <th>Type: </th> <td>?????</td> <th>Kingdom: </th> <td>Kg???</td> </tr> <tr> <th>Tier %:</th> <th>T1: ??%</th> <th>T2: ??%</th> <th>T3: ??%</th> <th>T4: ??%</th> <th>T5: ?%</th> </tr> <tr> <th>Monsters:</th> <th>????</th> <th>????</th> <th>????</th> <th>????</th> <th>????</th> </tr> </tbody> </table>';
+				newWindow.document.getElementById("navbar").innerHTML = '<table style="height: 100%;display: inline-block;position: absolute;left: 2.5%;width: 37.5%;"> <thead> <tr> <th colspan="2">Target Tile</th> <th colspan="2">Scale</th> </tr> </thead> <tbody> <tr> <th>X-Coord: </th> <td><input id="tileX" type="number" value="0"></td> <th>Scale: </th> <td><input id="scale" type="number" value="1"></td> </tr> <tr> <th>Y-Coord: </th> <td><input id="tileY" type="number" value="0"></td> <td>&nbsp;</td> <td>&nbsp;</td> </tr> </tbody> </table><table style="height: 100%;display: inline-block;position: absolute;left: 40%;width: 20%;"> <thead> <tr> <th colspan="5">Filter (Thresholds)</th> </tr> </thead> <tbody> <tr> <th>T1</th><th>T2</th><th>T3</th><th>T4</th><th>T5</th> </tr> <tr> <td><input id="t1" type="number" min="40" max="90" style="width:75%;" value="40"></td><td><input id="t2" type="number" min="20" max="60" style="width:75%;" value="20"></td><td><input id="t3" type="number" min="10" max="30" style="width:75%;" value="10"></td><td><input id="t4" type="number" min="5" max="15" style="width:75%;" value="5"></td><td><input id="t5" type="number" min="0" max="5" style="width:75%;" value="0"></td> </tr> </tbody></table> <table id="data" style="height: 100%;top: 0px;display: inline-block;position: absolute;right: 2.5%;width: 37.5%;"><thead> <tr> <th colspan="6">Tile Data</th> </tr> </thead> <tbody> <tr> <th>Coords: </th> <td>(388,408)</td> <th>Type: </th> <td>lake</td> <th>Kingdom: </th> <td></td> </tr> <tr> <th>Tier %:</th> <th>T1: 71%</th> <th>T2: 39%</th> <th>T3: ??%</th> <th>T4: ??%</th> <th>T5: ??%</th> </tr> <tr> <th>Monsters:</th> <th>250</th> <th>???</th> <th>???</th> <th>???</th> <th>???</th> </tr> </tbody></table>';
 			}
 			var newMap = newWindow.document.getElementById("newMap");
 			constructSVG(newMap, size);
@@ -151,12 +146,21 @@
 			newMap.removeEventListener("click", handleClick)
 			newWindow.document.getElementById("tileX").removeEventListener("change", handleX);
 			newWindow.document.getElementById("tileY").removeEventListener("change", handleY);
-			newWindow.document.getElementById("scale").removeEventListener("change", handleZoom);
+			newWindow.document.getElementById("t1").removeEventListener("change", handleFilter);
+			newWindow.document.getElementById("t2").removeEventListener("change", handleFilter);
+			newWindow.document.getElementById("t3").removeEventListener("change", handleFilter);
+			newWindow.document.getElementById("t4").removeEventListener("change", handleFilter);
+			newWindow.document.getElementById("t5").removeEventListener("change", handleFilter);
 			
 			newMap.addEventListener("click", handleClick);
 			newWindow.document.getElementById("tileX").addEventListener("change", handleX);
 			newWindow.document.getElementById("tileY").addEventListener("change", handleY);
 			newWindow.document.getElementById("scale").addEventListener("change", handleZoom);
+			newWindow.document.getElementById("t1").addEventListener("change", handleFilter);
+			newWindow.document.getElementById("t2").addEventListener("change", handleFilter);
+			newWindow.document.getElementById("t3").addEventListener("change", handleFilter);
+			newWindow.document.getElementById("t4").addEventListener("change", handleFilter);
+			newWindow.document.getElementById("t5").addEventListener("change", handleFilter);
 			
 			function handleClick(e){
 				if(e.toElement != newMap && e.target.attributes.name !== undefined){
@@ -166,25 +170,25 @@
 				e.stopImmediatePropagation();
 			}
 			function handleX(e){
-				var v = Number(e.target.value);
+				var v = ~~e.target.value;
 				if(v && v >= 0 && v <= 1000){
 					;
-					var x =  -(Number(e.target.value)+Number(newWindow.document.getElementById("tileY").value)/2)*size*2;
+					var x =  -(~~e.target.value + ~~newWindow.document.getElementById("tileY").value/2)*size*2;
 					newMap.currentTranslate.x = (scale*x)+newMap.width.animVal.value/2;
 				}
 				e.stopImmediatePropagation();
 			}
 			function handleY(e){
-				var v = Number(e.target.value);
+				var v = ~~e.target.value;
 				if(v && v >= 0 && v <= 1000){
-					var y =  -Number(v)*3*size/2;
+					var y =  -(~~v) *3*size/2;
 					newMap.currentTranslate.y = (scale*y)+newMap.height.animVal.value/2;
 				}
 				e.stopImmediatePropagation();
 			}
 			
 			function handleZoom(e){
-				var v = Number(e.target.value);
+				var v = ~~e.target.value;
 				if(v && v > 0 && v <= 20){
 					newMap.currentTranslate.x = ((newMap.currentTranslate.x-newMap.width.animVal.value/2 )*v/scale)+newMap.width.animVal.value/2;
 					newMap.currentTranslate.y = ((newMap.currentTranslate.y-newMap.height.animVal.value/2 )*v/scale)+newMap.height.animVal.value/2;;
@@ -197,6 +201,39 @@
 			function tileData(t){
 				var tile = map[t];
 				newWindow.document.getElementById("data").innerHTML = '<thead> <tr> <th colspan="6">Tile Data</th> </tr> </thead> <tbody> <tr> <th>Coords: </th> <td>(' + tile.x + ',' + tile.y+ ')</td> <th>Type: </th> <td>' + tile.type + '</td> <th>Kingdom: </th> <td>' + tile.kingdom + '</td> </tr> <tr> <th>Tier %:</th> <th>T1: ' + (tile.t1||"??") + '%</th> <th>T2: ' + (tile.t2||"??") + '%</th> <th>T3: ' + (tile.t3||"??") + '%</th> <th>T4: ' + (tile.t4||"??") + '%</th> <th>T5: ' + (tile.t5||"??") + '%</th> </tr> <tr> <th>Monsters:</th> <th>' +  (tile.monsters[0]||"???") + '</th> <th>' + (tile.monsters[1]||"???") + '</th> <th>' +  (tile.monsters[2]||"???") + '</th> <th>' +  (tile.monsters[3]||"???") + '</th> <th>' + (tile.monsters[4]||"???") + '</th> </tr> </tbody>'
+			}
+			
+			function handleFilter(e){
+				var t1 = ~~newWindow.document.getElementById("t1").value;
+				var t2 = ~~newWindow.document.getElementById("t2").value;
+				var t3 = ~~newWindow.document.getElementById("t3").value;
+				var t4 = ~~newWindow.document.getElementById("t4").value;
+				var t5 = ~~newWindow.document.getElementById("t5").value;
+				t1 = (t1 >= 40 && t1 <= 90) ? t1 : 40;
+				t2 = (t2 >= 20 && t2 <= 60) ? t2 : 20;
+				t3 = (t3 >= 10 && t3 <= 30) ? t3 : 10;
+				t4 = (t4 >= 5 && t4 <= 15) ? t4 : 5;
+				t5 = (t5 >= 0 && t5 <= 5) ? t5 : 0;
+				filter(t1,t2,t3,t4,t5);
+				e.stopImmediatePropagation();
+			}
+			
+			function filter(t1,t2,t3,t4,t5){
+				for(var t in map){
+					//If only let weren't strict mode specific...
+					/*console.log(t1 + ' ' + t2 + ' ' + t3 + ' ' + t4 + ' ' + t5);
+					console.log(tile);*/
+					var tile = map[t];
+					var element = newWindow.document.getElementById(t).getElementsByTagName("text")[0];
+					if((~~tile.t1 >= t1 || !tile.t1) && (~~tile.t2 >= t2 || !tile.t2) && (~~tile.t3 >= t3 || !tile.t3) && (~~tile.t4 >= t4 || !tile.t4) && (~~tile.t5 >= t5 || !tile.t5)){
+						element.style.fill = "hsl(300, 100%, 27%)";
+						
+						
+					}
+					else{
+						element.style.fill = "";
+					}
+				}
 			}
 	}
 	function display() {
